7c99113fbf0c0d54f1fde783bd59e176d99da61e
Stephen <stephen@geller.dev>
2026-04-14 15:10:31 +1000

    test

---
+++ test-file.ts
@@ -0,0 +1,14 @@
+function foo() {
+    console.log("bar")
+}
+
+/**
+ * Returns the plural form of a given word based on the provided count.
+ *
+ * @param num - The word to pluralize.
+ * @returns The word in its singular form if `count` is 1, otherwise the plural form.
+ */
+function bar(num: number) {
+    console.log(num)
+}
+
